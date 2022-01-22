5bfe95d66a5b41745ce5e7238c6302c96d06b436
Rudi Zengers <rudi@mb-rudi.home>
2022-01-22 22:49:32 +0100

    Added weather symbols

---
+++ include/WeatherSymbols.h
@@ -0,0 +1,18 @@
+/*
+ * This file contains the weather symbols as byte arrays in 50x50 Pixel format
+ * Red: 5 bit, Green: 6 bit, Blue: 5 bit
+ */
+#define  LV_COLOR_DEPTH 16
+#include "bmps/cstyle/bewolkt.c"
+#include "bmps/cstyle/bliksem.c"
+#include "bmps/cstyle/buien.c"
+#include "bmps/cstyle/hagel.c"
+#include "bmps/cstyle/halfbewolkt.c"
+#include "bmps/cstyle/helderenacht.c"
+#include "bmps/cstyle/mist.c"
+#include "bmps/cstyle/nachtmist.c"
+#include "bmps/cstyle/regen.c"
+#include "bmps/cstyle/sneeuw.c"
+#include "bmps/cstyle/wolkennacht.c"
+#include "bmps/cstyle/zonnig.c"
+#include "bmps/cstyle/zwaarbewolkt.c"
